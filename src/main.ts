2e9819bbe2c4d6d64e39e0b5d28ebe2412b07e88
Marina Nazina <nzyame@gmail.com>
2024-09-08 18:35:19 +0100

    ts

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-const age: number = 50;
-const username: string = 'Max';
-const toggle: boolean = true;
-const empty: null = null;
-const callback = (a: number): number => {
-	return 100 + a;
-};
-
-console.log(age, username, toggle, empty, callback);
